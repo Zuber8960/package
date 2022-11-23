f2238e87fffa44df479a1708907c0951cef5711e
Zuber8960 <zuberahmad8960@gmail.com>
2022-11-23 12:13:36 +0530

    first

---
+++ ndex.js
@@ -0,0 +1,25 @@
+
+
+// const fs = require('fs');
+// fs.writeFileSync('hi.txt', 'Code one to one');
+
+
+// let gs = require('fs').writeFileSync;
+// gs('hello.txt', 'new code in new file');
+
+
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.write('<H1>Hi, This is Zuber.</H1>');
+//     res.end();
+// }).listen(4091);
+
+const color=require('colors');
+
+console.log('Zuber'.green);
+// console.log('Life is not easy'.bgYellow);
+
+// import chalk from ('chalk');
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
